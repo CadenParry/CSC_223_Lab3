e32304dbde5ac36bc2313a7af40ced3a5412672a
CadenParry <cadenparry02@gmail.com>
2023-02-07 14:55:06 -0500

    LinkedList
    - need to work on previous() and reverse()

--- src/utilities/LinkedList.java
+++ src/utilities/LinkedList.java
@@ -1,5 +1,164 @@
 package utilities;
 
-public class LinkedList {
+import utilities.LinkedList.Node;
 
+public class LinkedList<T>
+{
+	protected Node 	_head;
+	protected Node	_tail;
+	protected int 	_size;
+
+	final class Node
+	{
+		public T 	_data;
+		public Node _next;
+
+		public Node() 
+		{
+			this(null, null);
+		}
+
+		public Node(T data, Node next) 
+		{
+			_data = data;
+			_next = next;
+		}
+	}
+
+	public LinkedList()
+	{
+		init();
+	}
+
+	private void init()
+	{
+		_head = new Node(null, null);
+		_tail = new Node(null, null);
+		_head._next = _tail;
+
+		_size = 0;
+	}
+
+	public boolean isEmpty()
+	{
+		return _size == 0;
+	}
+
+	public void clear()
+	{
+		init();
+	}
+
+	public int size()
+	{
+		return _size;
+	}
+
+	public void addToFront(T element)
+	{
+		if(element == null) return;
+		_head._next = new Node(element, _head._next);
+		_size += 1;
+	}
+
+	public boolean contains(T target)
+	{
+		return contains(target, _head._next);
+	}
+
+	private boolean contains(T target, Node n)
+	{
+		if(n == _tail) return false;
+		if(n._data.equals(target)) return true;
+		return contains(target, n._next);
+	}
+
+	public Node previous(T target)
+	{
+		return previous(target, _head);
+	}
+
+	private Node previous(T target, Node n)
+	{
+		if (n._next == _tail || n._next._data.equals(target)) 
+		{
+			return n != _tail ? n : null;
+		}
+		return previous(target, n._next);
+	}
+
+	public boolean remove(T target)
+	{
+		if(isEmpty())return false;
+
+		Node n = previous(target);
+		n._next = n._next._next;
+		_size -= 1;
+		return true;
+	}
+
+	public Node last()
+	{
+		if(isEmpty()) return _head;
+
+		Node n = _head._next;
+		while (n._next != _tail) 
+		{
+			n = n._next;
+		}
+		return n;
+	}
+
+	public void addToBack(T element)
+	{
+		Node n = last();
+		n._next = new Node(element, _tail);
+		_size += 1;
+	}
+
+	public String toString() 
+	{
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+		Node node = _head._next;
+		while (node != _tail) 
+		{
+			sb.append(node._data.toString());
+			sb.append(", ");
+			node = node._next;
+		}
+		if (_size > 0) 
+		{
+			sb.delete(sb.length()-2, sb.length());
+		}
+		sb.append("]");
+		return sb.toString();
+	}
+
+	//		public String toString()
+	//		{
+	//			String str = "";
+	//			for(Node current = _head._next; current != _tail; current = current._next)
+	//			{
+	//				str += current._data;
+	//			}
+	//			return str;
+	//		}
+
+	public void reverse()
+	{
+		if(isEmpty()) return;
+
+		Node firstHolder = last();
+		reverse(last(), previous(last()._data));
+		_head._next = firstHolder;
+	}
+
+	private void reverse(Node last, Node prev)
+	{
+		last._next = prev;
+		prev._next = _tail;
+		if (prev == _head._next) return;
+		reverse(last(), previous(prev._data));
+	}
 }
--- test/utilities/LinkedListTest.java
+++ test/utilities/LinkedListTest.java
@@ -7,8 +7,95 @@ import org.junit.jupiter.api.Test;
 class LinkedListTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	void testContainsPreviousRemove() 
+	{
+		LinkedList<String> list = new LinkedList<String>();
 
+		assertEquals(0, list._size);
+		assertTrue(list.isEmpty());
+		
+		list.addToFront("z");
+		list.addToFront("y");
+		list.addToFront("b");
+		list.addToFront("A");
+		list.addToFront("a");
+		list.addToFront("A");
+		
+		assertEquals(6, list._size);
+		assertFalse(list.isEmpty());
+
+		assertEquals("[A, a, A, b, y, z]", list.toString());
+		
+		assertFalse(list.contains(null));
+		assertTrue(list.contains("z"));
+		assertTrue(list.contains("a"));
+		assertTrue(list.contains("A"));
+		assertFalse(list.contains("cA"));
+		assertFalse(list.contains("Ac"));
+		
+		assertEquals("y", list.previous("z")._data);
+		assertEquals(list._head, list.previous("A"));
+		assertEquals("b", list.previous("y")._data);
+
+		assertTrue(list.remove("z"));
+		assertEquals("[A, a, A, b, y]", list.toString());
+		assertTrue(list.remove("A"));
+		assertEquals("[a, A, b, y]", list.toString());
+	}
+	
+	@Test
+	void testLast()
+	{
+		LinkedList<String> list = new LinkedList<String>();
+		assertEquals(list._head, list.last());
+
+		list.addToFront("z");
+		
+		assertEquals("z", list.last()._data);
+
+		list.addToFront("y");
+		
+		assertEquals("z", list.last()._data);
+
+		list.addToFront("b");
+		list.addToFront("A");
+		list.addToFront("a");
+		list.addToFront("A");
+		assertEquals("[A, a, A, b, y, z]", list.toString());
+		
+		assertEquals("z", list.last()._data.toString());
+	}
+	
+	@Test
+	void testAddToBack()
+	{
+		LinkedList<String> list = new LinkedList<String>();
+		
+		list.addToBack("A");
+		list.addToBack("a");
+		list.addToBack("A");
+		list.addToBack("b");
+		list.addToBack("y");
+		list.addToBack("z");
+		
+		assertEquals("[A, a, A, b, y, z]", list.toString());
+	}
+	
+	@Test
+	void testReverse()
+	{
+		LinkedList<String> list = new LinkedList<String>();
+		list.addToBack("A");
+		list.addToBack("a");
+		list.addToBack("B");
+		list.addToBack("b");
+		list.addToBack("C");
+		list.addToBack("c");
+		
+		System.out.println(list.toString());
+//		assertEquals("[A, a, B, b, C, c]", list.toString());
+		list.reverse();
+//		assertEquals("[c, C, b, B, a, A]", list.toString());
+		System.out.println(list.toString());
+	}
 }
